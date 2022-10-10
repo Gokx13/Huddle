f75b165ae2c129b7abf2fd365f243a4e5d88b9bf
Goku <gokulanand912@gmail.com>
2022-10-10 20:37:09 +0530

    commitig chages

---
+++ app.js
@@ -0,0 +1,12 @@
+ScrollReveal().reveal(".delay-200", { delay: 200 });
+ScrollReveal().reveal(".delay-225", { delay: 225 });
+ScrollReveal().reveal(".delay-250", { delay: 250 });
+ScrollReveal().reveal(".delay-300", { delay: 300 });
+ScrollReveal().reveal(".delay-450", { delay: 450 });
+ScrollReveal().reveal(".delay-700", { delay: 700 });
+ScrollReveal().reveal(".enter-top", { origin: "top" });
+ScrollReveal().reveal(".enter-right", { origin: "right" });
+ScrollReveal().reveal(".enter-left", { origin: "left" });
+ScrollReveal().reveal(".distance-135", { distance: "135px" });
+ScrollReveal().reveal(".distance-350", { distance: "350px" });
+ScrollReveal().reveal(".ease", { easing: "ease-out" });
---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        "very-dark-cyan": "hsl(192, 100%, 9%)",
+        "very-pale-blue": "hsl(207, 100%, 98%)",
+      },
+      colors: {
+        "primary-pink": "hsl(322, 100%, 66%)",
+        "light-pink": "hsl(321, 100%, 78%)",
+        "light-red": "hsl(0, 100%, 63%)",
+        "custom-grey": "#7D8E9B",
+      },
+      fontFamily: {
+        openSans: ["Open Sans", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+      spacing: {
+        "medium-p": "448px",
+        "large-p": "468px",
+        "xl-p": "589px",
+        "xl-container": "1360px",
+      },
+    },
+    screens: {
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1440px",
+      // => @media (min-width: 1440px) { ... }
+    },
+  },
+  plugins: [],
+};
